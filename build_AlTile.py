ae6b3ea34896336042e429cf7ed2dd09e3c7171b
Li <zepeng.li@protonmail.com>
2024-01-02 20:46:53 -0800

    fix a bug in read TH3.

--- build_AlTile.py
+++ build_AlTile.py
@@ -44,57 +44,55 @@ if __name__ == '__main__':
     for xbin in range(1, xaxis_new.GetNbins() + 1):
         x = XBins[xbin - 1]
         print('Processing file {}'.format(xbin - 1))
-        h_3d = read_wp('outputs/WP_3d_x{}.root'.format(xbin - 1))
-        for ybin in range(1, yaxis_new.GetNbins() + 1):
-            y = YBins[ybin - 1]
-            name = 'wp_x'+str(xbin)+'_y'+str(ybin)
-            wpHist = ROOT.TH1F(name, '', h_3d.GetNbinsZ() , ZBins)
-            print(y)
-            binx = 0
-            biny = 0
-            binz = 0
-            binx = xaxis_new.FindBin(x + 0.05)
-            biny = yaxis_new.FindBin(y + 0.05)
-            for k in range(1, zaxis_new.GetNbins() + 1):
-                wpHist.SetBinContent(k, 0)
-            for k in range(1, h_3d.GetNbinsZ() + 1):
-                wpHist.SetBinContent(k, h_3d.GetBinContent(xbin, ybin, k)*1e5)
-            #if wpHist.GetMaximum()> 0.9*1e5:
-            #    print(xbin, ybin)
-            if wpHist.GetMaximum() > 90000:
-                #wpHist.Fit('wpf')
-                p0 = wpf.GetParameter(0)
-                p1 = wpf.GetParameter(1)
-                p2 = wpf.GetParameter(2)
-                p3 = wpf.GetParameter(3)
-                p4 = wpf.GetParameter(4)
-                p5 = wpf.GetParameter(5)
-                p6 = wpf.GetParameter(6)
-                p7 = wpf.GetParameter(7)
-                print(p0, p1, p2, p3, p4, p5, p6, p7)
-            else:
-                p0 = 0
-                p1 = 0
-                p2 = 0
-                p3 = 0
-                p4 = 0
-                p5 = 0
-                p6 = 0
-                p7 = 0
-                wpf.SetParameter(0, 0)
-                wpf.SetParameter(1, 0)
-                wpf.SetParameter(2, 0)
-                wpf.SetParameter(3, 0)
-                wpf.SetParameter(4, 0)
-                wpf.SetParameter(5, 0)
-                wpf.SetParameter(6, 0)
-                wpf.SetParameter(7, 0)
-            #tg = ROOT.TGraph(wpHist)
-            #ts = ROOT.TSpline3('ts_x' + str(xbin)+ '_y' + str(ybin) , tg)
-            wpHist.Write()
-            #ts.Write()
-            if wpHist.GetMaximum()>90000:
-                for zbin in range(wpHist.GetNbinsX()):
-                    print(wpHist.GetBinCenter(zbin + 1), wpf.Eval(wpHist.GetBinCenter(zbin+1)), wpHist.GetBinContent(zbin+1))
-#            print xbin,'|',ybin
+        with ROOT.TFile.Open('outputs/WP_3d_x{}.root'.format(xbin - 1), 'READ') as f:
+            h_3d = f.Get('h_3D')
+            for ybin in range(1, yaxis_new.GetNbins() + 1):
+                y = YBins[ybin - 1]
+                name = 'wp_x'+str(xbin)+'_y'+str(ybin)
+                wpHist = ROOT.TH1F(name, '', h_3d.GetNbinsZ() , ZBins)
+                print(y)
+                binx = 0
+                biny = 0
+                binz = 0
+                binx = xaxis_new.FindBin(x + 0.05)
+                biny = yaxis_new.FindBin(y + 0.05)
+                for k in range(1, zaxis_new.GetNbins() + 1):
+                    wpHist.SetBinContent(k, 0)
+                for k in range(1, h_3d.GetNbinsZ() + 1):
+                    wpHist.SetBinContent(k, h_3d.GetBinContent(xbin, ybin, k)*1e5)
+                #if wpHist.GetMaximum()> 0.9*1e5:
+                #    print(xbin, ybin)
+                if wpHist.GetMaximum() > 90000:
+                    #wpHist.Fit('wpf')
+                    p0 = wpf.GetParameter(0)
+                    p1 = wpf.GetParameter(1)
+                    p2 = wpf.GetParameter(2)
+                    p3 = wpf.GetParameter(3)
+                    p4 = wpf.GetParameter(4)
+                    p5 = wpf.GetParameter(5)
+                    p6 = wpf.GetParameter(6)
+                    p7 = wpf.GetParameter(7)
+                    print(p0, p1, p2, p3, p4, p5, p6, p7)
+                else:
+                    p0 = 0
+                    p1 = 0
+                    p2 = 0
+                    p3 = 0
+                    p4 = 0
+                    p5 = 0
+                    p6 = 0
+                    p7 = 0
+                    wpf.SetParameter(0, 0)
+                    wpf.SetParameter(1, 0)
+                    wpf.SetParameter(2, 0)
+                    wpf.SetParameter(3, 0)
+                    wpf.SetParameter(4, 0)
+                    wpf.SetParameter(5, 0)
+                    wpf.SetParameter(6, 0)
+                    wpf.SetParameter(7, 0)
+                wpfile.WriteObject(wpHist, name)
+                #wpHist.Write()
+                if wpHist.GetMaximum()>90000:
+                    for zbin in range(wpHist.GetNbinsX()):
+                        print(wpHist.GetBinCenter(zbin + 1), wpf.Eval(wpHist.GetBinCenter(zbin+1)), wpHist.GetBinContent(zbin+1))
     wpfile.Close()
